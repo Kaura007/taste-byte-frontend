7d36e229f93e8f3568e13f4a0399a0039f8aa11a
Mohammed Abdulrahman kaura <81968559+Kaura007@users.noreply.github.com>
2026-01-29 12:01:05 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,4 @@ export const API_BASE_URL =
   import.meta.env.VITE_API_URL ||
   (import.meta.env.MODE === "development"
     ? "http://localhost:5000"
-    : https://taste-byte-backend-6qasj2ida-kaura007s-projects.vercel.app/");
+    : https://taste-byte-backend-6qasj2ida-kaura007s-projects.vercel.app");
